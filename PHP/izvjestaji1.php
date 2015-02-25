f3203dc0105c955879501b1a1896d90992ba8c34
Saša <sidrizov@gmail.com>
2015-02-25 19:14:42 +0100

    Update and rename PHP to PHP/izvjestaji1.php

---
+++ PHP/izvjestaji1.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<html>
+       <button type="button"><a href="izvjestaji.php">Izvjestaj o obavljenim tretmanima radnika </a></button><br>
+  <button type="button"><a href="izvjestaji3.php">Izvjestaj o prodanim proizvodima radnika</a></button><br>
+<button type="button"><a href="glavna.php">Nazad na glavnu</a></button><br>
+<button type="button"><a href="spa.php">Spa usluge</a></button><br>
+</html>
