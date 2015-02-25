cc0229ba4923ef49ca6121a92b809f027710b783
Saša <sidrizov@gmail.com>
2015-02-25 19:20:33 +0100

    Update and rename PHP to PHP/sesija4.php

---
+++ PHP/sesija4.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<form action="sesija.php" method="GET">
+<input type="submit" name="vrsta" value="Njega" />
+ <input type="submit" name="vrsta" value="Masaza" />
+ <input type="submit" name="vrsta" value="Man Space" />
+<input type="submit" name="vrsta" value="Depilacija-wax" />
+<input type="submit" name="vrsta" value="Depilacija-secer" />
+<input type="submit" name="vrsta" value="Manikura" />
+
+</form>
