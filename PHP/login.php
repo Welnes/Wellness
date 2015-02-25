28de68d33e7f8c3d09f63e7d0a4c8f34b371bdbf
Saša <sidrizov@gmail.com>
2015-02-25 19:16:31 +0100

    Update and rename PHP to PHP/login.php

---
+++ PHP/login.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<html>
+<form action='login2.php'  method='POST'>
+<input type='text' name='username' ><br>
+<input type='password' name='lozinka'><br>
+<input type='submit' value='Logiraj me'><br>
+</html>
